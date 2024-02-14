772c0da9fc19718bf4f200f77eab54fad925abe7
Fredrik Hov Nilsen <nfredrikhov@gmail.com>
2024-02-14 22:04:01 +0100

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import express, { Express, response } from "express";
-import expressCallback from "./expressCallback";
+import express, { Express } from "express";
+import expressCallback from "./middleware/expressCallback";
 import { makeDbConnection, blogsDb } from "./data-access";
-import { listBlogs } from "./use-cases";
 import { getBlogs } from "./controllers";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const app: Express = express();
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -18,13 +17,12 @@ app.get("/", expressCallback(getBlogs));
 const start = async () => {
   await makeDbConnection()
     .then(() => {
-      console.log("Promise resolved, starting service");
       app.listen(port, () => {
         console.log(`Server is runnning at -> http://localhost:${port}`);
       });
     })
     .catch(() => {
-      console.log("Cant establish DB connecting || server");
+      console.log("Cant establish DB connection");
     });
 };
 start();
--- src/use-cases/listBlogs.ts
+++ src/use-cases/listBlogs.ts
@@ -1,6 +1,5 @@
 export default function makeListBlogs({ blogsDb }) {
   const listBlogs = async () => {
-    console.log("use case running");
     const blogs = (await blogsDb).findAllBlogs();
     return blogs;
   };
